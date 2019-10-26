a668af1bbb45571808f42238cb79c6547b950631
Glitch (hello-express) <none>
2019-10-26 20:51:41 +0000

    🌁🚩 Checkpoint ./server.js:782306/27826

--- server.js
+++ server.js
@@ -11,40 +11,40 @@ const slackEvents = createEventAdapter(slackSigningSecret);
 
 const app = express();
 const uri = 'mongodb+srv://admin:root@cluster0-vucd7.mongodb.net/test?retryWrites=true&w=majority'
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-  if(err) throw err;
-  const usersCollection = client.db("bugstack").collection("users");
-  console.log('cool')
-  app.use(express.static("public"));
+MongoClient.connect(uri, (err, client) => {
 
-  app.use('/slack_event', slackEvents.expressMiddleware());
+  
+});
 
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(bodyParser.json());
+app.use(express.static("public"));
 
-  slackEvents.on('message', (event) => {
-    console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
-  });
+app.use('/slack_event', slackEvents.expressMiddleware());
 
-  app.post("/relay_bug", (req, res) => {
-    console.log(req.body)
-    res.end('')
-  });
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-  app.get("/", function(request, response) {
-    response.sendFile(__dirname + "/views/index.html");
-  });
+slackEvents.on('message', (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
 
-  app.get("/sw", (req, res) => {
-    res.sendFile(__dirname + "/public/sw.js");
-  })
+app.post("/relay_bug", (req, res) => {
+  console.log(req.body);
+  res.end('');
+});
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/sw", (req, res) => {
+  res.sendFile(__dirname + "/public/sw.js");
+})
 
   app.get("/slack_auth", (req, res)=>{
-    console.log(process.env.CLIENT_ID)
+    console.log(process.env.CLIENT_ID);
     res.redirect(`https://slack.com/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=bot%20channels:history%20chat:write:bot&redirect_uri=https://time-waterlily.glitch.me/slack_callback`);
   });
 
@@ -72,11 +72,10 @@ client.connect(err => {
   app.post('/slack_event', (req,res)=>{
     res.end(req.body.challenge);
   });
-  client.close();
-});
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+  const listener = app.listen(process.env.PORT, function() {
+    console.log("Your app is listening on port " + listener.address().port);
+  });
+
 
 
