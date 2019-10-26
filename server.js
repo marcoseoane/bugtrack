68fa6df48b1338f476e7ce1c95900bb0fa110a3f
Glitch (hello-express) <none>
2019-10-26 21:03:41 +0000

    🚔🐪 Checkpoint ./server.js:782306/2248

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ const slackEvents = createEventAdapter(slackSigningSecret);
 
 const app = express();
 const uri = 'mongodb+srv://admin:root@cluster0-vucd7.mongodb.net/test?retryWrites=true&w=majority'
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-MongoClient.connect(uri, (err, client) => {
+let db;
 
-  
+client.connect(err => {
+  if(err) throw err;
+  db = client.db('bugtrack')
+  client.close();
 });
 
 app.use(express.static("public"));
@@ -41,41 +45,55 @@ app.get("/", function(request, response) {
 
 app.get("/sw", (req, res) => {
   res.sendFile(__dirname + "/public/sw.js");
-})
+});
 
-  app.get("/slack_auth", (req, res)=>{
-    console.log(process.env.CLIENT_ID);
-    res.redirect(`https://slack.com/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=bot%20channels:history%20chat:write:bot&redirect_uri=https://time-waterlily.glitch.me/slack_callback`);
-  });
+app.get("/slack_auth", (req, res)=>{
+  console.log(process.env.CLIENT_ID);
+  res.redirect(`https://slack.com/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=bot%20channels:history%20chat:write:bot&redirect_uri=https://time-waterlily.glitch.me/slack_callback`);
+});
 
-  app.get("/slack_callback", (req, res)=>{
-    axios.post('https://slack.com/api/oauth.access', qs.stringify({
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      code: req.query.code
-    }), 
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
-    .then(function (response) {
-      console.log(response.data);
-      res.end('');
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.end('error');
-    });
+app.get("/slack_callback", (req, res)=>{
+  axios.post('https://slack.com/api/oauth.access', qs.stringify({
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code: req.query.code
+  }), 
+  {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+  .then(async function (response) {
+    const { access_token, user_id, team_id, enterprise_id, team_name, bot } = response.data;
+    const newUser = {
+      slack_token: access_token,
+      user_id,
+      team_id,
+      enterprise_id,
+      team_name,
+      bot_user_id: bot.bot_user_id,
+      bot_token: bot.access_token
+    };
+    
+    const existingUser = await db.collection('users').findOne({user_id: user_id});
+    
+    console.log(existingUser)
+    
+    res.end('');
+  })
+  .catch(function (error) {
+    console.log(error);
+    res.end('error');
   });
+});
 
-  app.post('/slack_event', (req,res)=>{
-    res.end(req.body.challenge);
-  });
+app.post('/slack_event', (req,res)=>{
+  res.end(req.body.challenge);
+});
 
-  const listener = app.listen(process.env.PORT, function() {
-    console.log("Your app is listening on port " + listener.address().port);
-  });
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
 
 
 
